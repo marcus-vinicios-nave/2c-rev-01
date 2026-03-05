8bd616879571a554e8d192b7d48eeeb96f936173
DaviBarboza98 <davi.barboza@nave.org.br>
2026-03-05 10:01:02 -0300

    ex-01-variaveis-nomes.js

--- ex-01-variaveis-nomes.js
+++ ex-01-variaveis-nomes.js
@@ -68,62 +68,62 @@ MÍNIMO DE RESPOSTAS NECESSÁRIAS: 8
 */
 
 /* Questão 01 */
-export let funçãoImportante = true
+export let funcaoImportante = true
 
 /* Questão 02 */
-export class clientes_do_salão {}
+export class ClientesDoSalao {}
 
 /* Questão 03 */
-export let próximaEtapa = true
+export let proximaEtapa = true
 
 /* Questão 04 */
-export const data_e_horario = true
+export const dataEHorario = true
 
 /* Questão 05 */
-export const vídeoAula = true
+export const videoAula = true
 
 /* Questão 06 */
-export let arquivo_principal = true
+export let arquivoPrincipal = true
 
 /* Questão 07 */
-export class CômodosDaCasa {}
+export class ComodosDaCasa {}
 
 /* Questão 08 */
-export let $4rodas = true
+export let quatroRodas = true
 
 /* Questão 09 */
-export let _5PowerRangers = true
+export let cincoPowerRangers = true
 
 /* Questão 10 */
-export let puro_Aço = true
+export let puroAco = true
 
 /* Questão 11 */
-export const $100reaisEmBarras_de_OURO = true
+export const cemReaisEmBarrasDeOuro = true
 
 /* Questão 12 */
-export const páginaInicial = true
+export const paginaInicial = true
 
 /* Questão 13 */
-export let _5patinhosForamPassear = true
+export let cincoPatinhosForamPassear = true
 
 /* Questão 14 */
-export class $1besouro {}
+export class UmBesouro {}
 
 /* Questão 15 */
-export class Roupas_Juninas {}
+export class RoupasJuninas {}
 
 /* Questão 16 */
-export let cálculosMatemáticos = true
+export let calculosMatematicos = true
 
 /* Questão 17 */
-export const camaMesa_E_Banho = true
+export const camaMesaEBanho = true
 
 /* Questão 18 */
-export let preçoDOproduto = true
+export let precoDoProduto = true
 
 /* Questão 19 */
-export const código_fonte = true
+export const codigoFonte = true
 
 /* Questão 20 */
-export class Cabo_frio {}
+export class CaboFrio {}
 
